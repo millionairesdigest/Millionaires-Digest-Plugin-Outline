de16aa3cdcab38eeeb7945d969b18c9cb2925d4e
K&L <37118190+millionairesdigest@users.noreply.github.com>
2019-01-16 10:44:55 -0500

    Create plugin-name.php

---
+++ plugin-name.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Millionaire's Digest
+Description: This is an example sentence.
+Version: 1.0.0
+Author: K&L (Founder of the Millionaire's Digest)
+Author URI: https://millionairedigest.com/
+*/
